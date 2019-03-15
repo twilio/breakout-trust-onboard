316997ae98c2b1e2e1f6a97e0395125e9095d75f
Randy Beiter <rbeiter@twilio.com>
2019-03-15 10:40:23 -0400

    Added service to bring up ppp connection

--- device-support/Seeed-LTE_Cat_1_Pi_HAT/install_service.sh
+++ device-support/Seeed-LTE_Cat_1_Pi_HAT/install_service.sh
@@ -5,6 +5,9 @@ if [ ! -d /usr/local/seeed_lte_hat ]; then
 fi
 cp service/seeed_lte_hat.py /usr/local/seeed_lte_hat/
 cp service/seeed_lte_hat.service /etc/systemd/system/
+cp service/twilio_ppp.service /etc/systemd/system/
 systemctl enable seeed_lte_hat
+systemctl enable twilio_ppp
 systemctl daemon-reload
 systemctl start seeed_lte_hat
+systemctl start twilio_ppp
