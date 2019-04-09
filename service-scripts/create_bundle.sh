e5b4d3a1443875c70f1e17e53a8b46ec0ea06d4d
Randy Beiter <rbeiter@twilio.com>
2019-04-08 20:21:53 -0400

    Updates for build process

--- service-scripts/create_bundle.sh
+++ service-scripts/create_bundle.sh
@@ -16,7 +16,8 @@ WIRELESS_PPP_REPO=https://github.com/twilio/wireless-ppp-scripts.git
 WIRELESS_PPP_BRANCH=master
 
 RASPBIAN_DISTRO_URL=http://raspbian.raspberrypi.org/raspbian/
-REQUIRED_PACKAGES="libcurl4-openssl-dev libpcap0.8 libssl1.0-dev ppp uuid-dev cmake cmake-data libarchive13 libjsoncpp1 libuv1 liblzo2-2"
+REQUIRED_PACKAGES="libcurl4-openssl-dev libpcap0.8 libssl1.0-dev ppp uuid-dev cmake cmake-data libarchive13 libjsoncpp1 libuv1 liblzo2-2 smstools procmail screen"
+REQUIRED_PACKAGES="${REQUIRED_PACKAGES} udhcpd"
 
 find_file () {
 	package=$1
@@ -62,7 +63,7 @@ while getopts 'hm:i:a:b:r:p:' opt; do
 		a)	azure_repo=${OPTARG} ;;
 		b)	tob_repo=${OPTARG} ;;
 		r)	rpi_tools_repo=${OPTARG} ;;
-		r)	wireless_ppp_repo=${OPTARG} ;;
+		p)	wireless_ppp_repo=${OPTARG} ;;
 	esac
 done
 
@@ -116,7 +117,7 @@ echo "Downloading Debian packages"
 mkdir -p debs
 pushd debs
 for package in ${REQUIRED_PACKAGES}; do
-	wget "${RASPBIAN_DISTRO_URL}$(find_file ${package})"
+	wget -nc "${RASPBIAN_DISTRO_URL}$(find_file ${package})"
 done
 popd
 
@@ -131,6 +132,21 @@ export RPI_ROOT=${mount_point}
 
 echo "Building Azure SDK"
 pushd ${azure_repo}/build_all/linux
+patch -p3 -N << _DONE_
+diff --git a/build_all/linux/build.sh b/build_all/linux/build.sh
+index 15a65bce0..9d5c60a8a 100755
+--- a/build_all/linux/build.sh
++++ b/build_all/linux/build.sh
+@@ -127,7 +127,7 @@ process_args \$*
+ rm -r -f \$build_folder
+ mkdir -p \$build_folder
+ pushd \$build_folder
+-cmake \$toolchainfile \$cmake_install_prefix -Drun_valgrind:BOOL=\$run_valgrind -DcompileOption_C:STRING="\$extracloptions" -Drun_e2e_tests:BOOL=\$run_e2e_tests -Drun_sfc_tests:BOOL=\$run-sfc-tests -Drun_longhaul_tests=\$run_longhaul_tests -Duse_amqp:BOOL=\$build_amqp -Duse_http:BOOL=\$build_http -Duse_mqtt:BOOL=\$build_mqtt -Ddont_use_uploadtoblob:BOOL=\$no_blob -Drun_unittests:BOOL=\$run_unittests -Dbuild_python:STRING=\$build_python -Dno_logging:BOOL=\$no_logging \$build_root -Duse_prov_client:BOOL=\$prov_auth -Duse_tpm_simulator:BOOL=\$prov_use_tpm_simulator -Duse_edge_modules=\$use_edge_modules
++cmake \$toolchainfile \$cmake_install_prefix -Drun_valgrind:BOOL=\$run_valgrind -DcompileOption_C:STRING="\$extracloptions" -Drun_e2e_tests:BOOL=\$run_e2e_tests -Drun_sfc_tests:BOOL=\$run-sfc-tests -Drun_longhaul_tests=\$run_longhaul_tests -Duse_amqp:BOOL=\$build_amqp -Duse_http:BOOL=\$build_http -Duse_mqtt:BOOL=\$build_mqtt -Ddont_use_uploadtoblob:BOOL=\$no_blob -Drun_unittests:BOOL=\$run_unittests -Dbuild_python:STRING=\$build_python -Dno_logging:BOOL=\$no_logging \$build_root -Duse_prov_client:BOOL=\$prov_auth -Duse_tpm_simulator:BOOL=\$prov_use_tpm_simulator -Duse_edge_modules=\$use_edge_modules -Duse_openssl:bool=ON -Dbuild_provisioning_service_client=ON
+ 
+ if [ "$make" = true ]
+ then
+_DONE_
 ./build.sh --toolchain-file ${tob_repo}/device-support/Seeed-LTE_Cat_1_Pi_HAT/toolchain.cmake --provisioning --no_uploadtoblob --install-path-prefix ${RPI_ROOT}/usr
 cd ../../cmake/iotsdk_linux
 sudo make install
@@ -164,12 +180,32 @@ popd
 
 sudo ln -s /etc/systemd/system/twilio_ppp.service ${RPI_ROOT}/etc/systemd/system/multi-user.target.wants/twilio_ppp.service
 
+echo "Setting up smstools"
+sudo cp bundle_files/etc/smsd.conf ${RPI_ROOT}/etc/
+sudo cp bundle_files/home/pi/sms_received.sh ${RPI_ROOT}/home/pi/
+sudo touch ${RPI_ROOT}/home/pi/azure_scope_id.txt
+pi_uname=$(cat ${mount_point}/etc/passwd | grep '^pi:' | cut -d ':' -f 3)
+dialout_grp=$(cat ${mount_point}/etc/group | grep '^dialout:' | cut -d ':' -f 3)
+sudo chown -R ${pi_uname}:${dialout_grp} ${mount_point}/home/pi/azure_scope_id.txt
+
+echo "Setting up udhcpd"
+sudo cp bundle_files/etc/udhcpd.conf ${RPI_ROOT}/etc/
+
+echo "Setting up static IP address for conference"
+sudo cp bundle_files/etc/network/interfaces ${RPI_ROOT}/etc/network/
+
+# TODO: enable sshd
+# TODO: change default pi password
+# TODO: change default locale for keyboard to en_US.UTF-8 UTF-8
+# TODO: /usr/local/seeed_lte_hat/ directory and py file are missing
+
 echo "Installing sample sources"
 sudo rm -rf ${mount_point}/home/pi/azure-sample
 sudo cp -r ${tob_repo}/cloud-support/azure-iot ${mount_point}/home/pi/azure-sample
+sudo rsync -axvp ${tob_repo} ${mount_point}/home/pi/Breakout_Trust_Onboard_SDK
 pi_uname=$(cat ${mount_point}/etc/passwd | grep '^pi:' | cut -d ':' -f 3)
 pi_grp=$(cat ${mount_point}/etc/passwd | grep '^pi:' | cut -d ':' -f 4)
-sudo chown -R ${pi_uname}:${pi_grp} ${mount_point}/home/pi/azure-sample
+sudo chown -R ${pi_uname}:${pi_grp} ${mount_point}/home/pi/{azure-sample,Breakout_Trust_Onboard_SDK}
 
 sudo umount ${mount_point}
 rmdir ${mount_point}
