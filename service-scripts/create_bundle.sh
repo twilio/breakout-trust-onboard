5b100bdbc5fa3df0c4b11103137dae8a0e51ccbe
Anton Gerasimov <agerasimov@twilio.com>
2019-04-10 19:28:56 +0200

    Fixes in create_bundle.sh
    * Explicit error handling or ignoring where appropriate
    * Cleanup hook
    * Fixed several paths

--- service-scripts/create_bundle.sh
+++ service-scripts/create_bundle.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 DEFAULT_RASPBIAN_URL=https://downloads.raspberrypi.org/raspbian_lite_latest
 DEFAULT_RASPBIAN_IMAGENAME=raspbian_lite_latest
@@ -10,7 +10,7 @@ RPI_TOOLS_REPO=https://github.com/raspberrypi/tools.git
 RPI_TOOLS_BRANCH=master
 
 TOB_REPO=git@github.com:twilio/Breakout_Trust_Onboard_SDK.git
-TOB_BRANCH=initial-implementation
+TOB_BRANCH=build-bundle
 
 WIRELESS_PPP_REPO=https://github.com/twilio/wireless-ppp-scripts.git
 WIRELESS_PPP_BRANCH=master
@@ -30,6 +30,14 @@ find_file () {
 	sed -n "s/Filename: \(.*\)/\1/p"
 }
 
+mount_cleanup () {
+	sudo umount ${RPI_BOOT}
+	sudo umount ${RPI_ROOT}
+	sudo losetup -d ${loop_device}
+	rmdir ${RPI_BOOT}
+	rmdir ${RPI_ROOT}
+}
+
 mount_sysroot () {
 	mountpoint_boot=$1
 	mountpoint_root=$2
@@ -37,12 +45,17 @@ mount_sysroot () {
 
 	loop_device=$(sudo losetup --show -f "${image}")
 	export loop_device
-	sudo partprobe ${loop_device}
-	sudo mount ${loop_device}p1 ${mountpoint_boot}
+	sudo partprobe ${loop_device} && \
+	sudo mount ${loop_device}p1 ${mountpoint_boot} && \
 	sudo mount ${loop_device}p2 ${mountpoint_root}
 }
 
-# parted,wget?,dpkg,
+fail () {
+	message=$1
+
+	echo "Failed to create image: $message"
+	exit 1
+}
 
 while getopts 'hn:m:i:a:b:r:p:' opt; do
 	case ${opt} in
@@ -81,26 +94,29 @@ fi
 if [ -z "$raspbian_image" ]; then
 	echo "Downloading Raspbian image... "
 	if [ -f ${DEFAULT_RASPBIAN_IMAGENAME} ]; then
-		wget -c ${DEFAULT_RASPBIAN_URL}
+		wget -c ${DEFAULT_RASPBIAN_URL} || true
 	else
-		wget ${DEFAULT_RASPBIAN_URL}
+		wget ${DEFAULT_RASPBIAN_URL} || fail "couldn't download Raspbian"
 	fi
 
 	unpacked_image_name=$(unzip -l ${DEFAULT_RASPBIAN_IMAGENAME} | sed -n 's/^\s*[^ \t]*\s*[^ \t]*\s[^ \t]*\s*\([^ \t]*\.img\).*$/\1/p')
-	echo "Extracting Raspbian image... "
-	unzip -f ${DEFAULT_RASPBIAN_IMAGENAME}
+
+	if [ ! -f ${unpacked_image_name} ]; then
+		echo "Extracting Raspbian image... "
+		unzip -u ${DEFAULT_RASPBIAN_IMAGENAME} || fail "couldn't unzip Raspbian"
+	fi
 	raspbian_image=$(pwd)/${unpacked_image_name}
 fi
 
 
 if [ -z "$azure_repo" ]; then
   if [ ! -d azure-sdk ]; then
-	  echo "Cloning Azure IoT SDK... "
+	echo "Cloning Azure IoT SDK... "
   	git clone "$AZURE_REPO" --branch ${AZURE_BRANCH} --recursive azure-sdk
   else
-    echo "Using existing Azure IoT SDK."
+	echo "Using existing Azure IoT SDK."
   fi
-	azure_repo=$(pwd)/azure-sdk
+  azure_repo=$(pwd)/azure-sdk
 fi
 
 if [ -z "$tob_repo" ]; then
@@ -110,7 +126,7 @@ if [ -z "$tob_repo" ]; then
   else
 	  echo "Using existing Trust Onboard SDK... "
   fi
-	tob_repo=$(pwd)/tob-sdk
+  tob_repo=$(pwd)/tob-sdk
 fi
 
 if [ -z "$rpi_tools_repo" ]; then
@@ -120,7 +136,7 @@ if [ -z "$rpi_tools_repo" ]; then
   else
 	  echo "Using existing Raspberry Pi tools... "
   fi
-	rpi_tools_repo=$(pwd)/rpi-tools
+  rpi_tools_repo=$(pwd)/rpi-tools
 fi
 
 if [ -z "$wireless_ppp_repo" ]; then
@@ -130,12 +146,13 @@ if [ -z "$wireless_ppp_repo" ]; then
   else
   	echo "Using existing Wireless PPP scripts... "
   fi
-	wireless_ppp_repo=$(pwd)/wireless-ppp
+  wireless_ppp_repo=$(pwd)/wireless-ppp
 fi
 
-mount_sysroot ${mount_point_boot} ${mount_point_root} ${raspbian_image}
+mount_sysroot ${mount_point_boot} ${mount_point_root} ${raspbian_image} || fail "couldn't mount sysroot"
 export RPI_BOOT=${mount_point_boot}
 export RPI_ROOT=${mount_point_root}
+trap mount_cleanup INT TERM EXIT
 
 if [ -z "${RPI_BOOT}" ]; then
   echo "Error determining RPI_BOOT '${RPI_BOOT}' - aborting."
@@ -152,14 +169,22 @@ echo "Downloading Debian packages"
 mkdir -p debs
 pushd debs
 for package in ${REQUIRED_PACKAGES}; do
-	wget -nc "${RASPBIAN_DISTRO_URL}$(find_file ${package})"
+	package_path=$(find_file ${package})
+
+	if [ -z "$package_path" ]; then
+		fail "couldn't find ${package} in Raspbian repo"
+	fi
+
+	if [ ! -f $(basename ${package_path}) ]; then
+		wget -nc "${RASPBIAN_DISTRO_URL}${package_path}" || fail "couldn't download ${package} from ${RASPBIAN_DISTRO_URL}${package_path}"
+	fi
 done
 popd
 
 echo "Installing Debian packages"
 
 for package in debs/*.deb; do
-	sudo dpkg -x ${package} ${RPI_ROOT}
+	sudo dpkg -x ${package} ${RPI_ROOT} || fail "couldn't install ${package}"
 done
 
 export PATH=${PATH}:${rpi_tools_repo}/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin
@@ -167,7 +192,7 @@ export PATH=${PATH}:${rpi_tools_repo}/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf
 echo "Building Azure SDK"
 pushd ${azure_repo}/build_all/linux
 set +e
-patch -p3 -N << _DONE_
+patch -p3 -N <<_DONE_ || true
 diff --git a/build_all/linux/build.sh b/build_all/linux/build.sh
 index 15a65bce0..9d5c60a8a 100755
 --- a/build_all/linux/build.sh
@@ -182,25 +207,26 @@ index 15a65bce0..9d5c60a8a 100755
  if [ "$make" = true ]
  then
 _DONE_
+
 set -e
-./build.sh --toolchain-file ${tob_repo}/device-support/Seeed-LTE_Cat_1_Pi_HAT/toolchain.cmake --provisioning --no_uploadtoblob --install-path-prefix ${RPI_ROOT}/usr
+./build.sh --toolchain-file ${tob_repo}/device-support/Seeed-LTE_Cat_1_Pi_HAT/toolchain.cmake --provisioning --no_uploadtoblob --install-path-prefix ${RPI_ROOT}/usr || fail "failed to build Azure SDK"
 cd ../../cmake/iotsdk_linux
-sudo make install
+sudo make install || fail "failed to install Azure SDK"
 popd
 
 echo "Building Trust Onboard SDK"
 pushd ${tob_repo}
 mkdir -p cmake
 cd cmake
-cmake -DCMAKE_TOOLCHAIN_FILE=${tob_repo}/device-support/Seeed-LTE_Cat_1_Pi_HAT/toolchain.cmake -DTOB_DEVICE=Seeed-LTE_Cat_1_Pi_HAT ..
-cpack
+cmake -DCMAKE_TOOLCHAIN_FILE=${tob_repo}/device-support/Seeed-LTE_Cat_1_Pi_HAT/toolchain.cmake -DTOB_DEVICE=Seeed-LTE_Cat_1_Pi_HAT .. || fail "failed to build Trust Onboard SDK"
+cpack || fail "failed to package Trust Onboard SDK"
 for package in *.deb; do
-	sudo dpkg -x ${package} ${RPI_ROOT}
+	sudo dpkg -x ${package} ${RPI_ROOT} || fail "failed to install Trust Onboard SDK"
 done
 
 popd
 
-sudo ln -s /etc/systemd/system/seeed_lte_hat.service ${RPI_ROOT}/etc/systemd/system/multi-user.target.wants/seeed_lte_hat.service
+sudo ln -fs /etc/systemd/system/seeed_lte_hat.service ${RPI_ROOT}/etc/systemd/system/multi-user.target.wants/seeed_lte_hat.service
 
 echo "Installing PPP scripts"
 ppp_tempdir=$(mktemp -d)
@@ -214,11 +240,11 @@ sudo cp peers/twilio ${RPI_ROOT}/etc/ppp/peers
 rm -rf ${ppp_tempdir}
 popd
 
-sudo ln -s /etc/systemd/system/twilio_ppp.service ${RPI_ROOT}/etc/systemd/system/multi-user.target.wants/twilio_ppp.service
+sudo ln -fs /etc/systemd/system/twilio_ppp.service ${RPI_ROOT}/etc/systemd/system/multi-user.target.wants/twilio_ppp.service
 
 echo "Setting up smstools"
-sudo cp bundle_files/etc/smsd.conf ${RPI_ROOT}/etc/
-sudo cp bundle_files/home/pi/sms_received.sh ${RPI_ROOT}/home/pi/
+sudo cp ${tob_repo}/service-scripts/bundle_files/etc/smsd.conf ${RPI_ROOT}/etc/
+sudo cp ${tob_repo}/service-scripts/bundle_files/home/pi/sms_received.sh ${RPI_ROOT}/home/pi/
 sudo touch ${RPI_ROOT}/home/pi/azure_id_scope.txt
 pi_uname=$(cat ${RPI_ROOT}/etc/passwd | grep '^pi:' | cut -d ':' -f 3)
 dialout_grp=$(cat ${RPI_ROOT}/etc/group | grep '^dialout:' | cut -d ':' -f 3)
@@ -228,11 +254,11 @@ echo "Configuring default locale"
 # TODO: change default locale to en_US.UTF-8 UTF-8
 # TODO: additional work needed -> mnt/rootfs/usr/lib/locale/locale-archive /media/rbeiter/rootfs/usr/lib/locale/locale-archive
 # -bash: warning: setlocale: LC_ALL: cannot change locale (en_US.utf8)
-sudo cp bundle_files/etc/default/locale ${RPI_ROOT}/etc/default/
-sudo cp bundle_files/etc/locale.gen ${RPI_ROOT}/etc/
+sudo cp ${tob_repo}/service-scripts/bundle_files/etc/default/locale ${RPI_ROOT}/etc/default/
+sudo cp ${tob_repo}/service-scripts/bundle_files/etc/locale.gen ${RPI_ROOT}/etc/
 
 echo "Configuring default keyboard layout"
-sudo cp bundle_files/etc/default/keyboard ${RPI_ROOT}/etc/default/
+sudo cp ${tob_repo}/service-scripts/bundle_files/etc/default/keyboard ${RPI_ROOT}/etc/default/
 
 # TODO: change default pi password
 
@@ -247,39 +273,33 @@ sudo sed -i 's/^#\(dtparam=i2c_arm=on\)/\1/g' ${RPI_BOOT}/config.txt
 # TODO: Conference-only configuration, will be removed for official Critical IoT Kit
 
 echo "Enabling sshd by default for conference"
-sudo rm ${RPI_ROOT}/etc/init/ssh.override
-sudo ln -s /lib/systemd/system/ssh.service ${RPI_ROOT}/etc/systemd/system/multi-user.target.wants/ssh.service
-sudo mv ${RPI_ROOT}/etc/rc2.d/{K,S}01ssh
-sudo mv ${RPI_ROOT}/etc/rc3.d/{K,S}01ssh
-sudo mv ${RPI_ROOT}/etc/rc4.d/{K,S}01ssh
-sudo mv ${RPI_ROOT}/etc/rc5.d/{K,S}01ssh
+sudo rm -f ${RPI_ROOT}/etc/init/ssh.override
+sudo ln -fs /lib/systemd/system/ssh.service ${RPI_ROOT}/etc/systemd/system/multi-user.target.wants/ssh.service
+sudo mv ${RPI_ROOT}/etc/rc2.d/{K,S}01ssh || true
+sudo mv ${RPI_ROOT}/etc/rc3.d/{K,S}01ssh || true
+sudo mv ${RPI_ROOT}/etc/rc4.d/{K,S}01ssh || true
+sudo mv ${RPI_ROOT}/etc/rc5.d/{K,S}01ssh || true
 
 echo "Setting up udhcpd"
 sudo sed -i 's/ENABLED="no"/ENABLED="yes"/' ${RPI_ROOT}/etc/default/udhcpd
-sudo cp bundle_files/etc/udhcpd.conf ${RPI_ROOT}/etc/
-sudo ln -s ../init.d/udhcpd ${RPI_ROOT}/etc/rc0.d/K01udhcpd 
-sudo ln -s ../init.d/udhcpd ${RPI_ROOT}/etc/rc1.d/K01udhcpd 
-sudo ln -s ../init.d/udhcpd ${RPI_ROOT}/etc/rc2.d/S01udhcpd 
-sudo ln -s ../init.d/udhcpd ${RPI_ROOT}/etc/rc3.d/S01udhcpd 
-sudo ln -s ../init.d/udhcpd ${RPI_ROOT}/etc/rc4.d/S01udhcpd 
-sudo ln -s ../init.d/udhcpd ${RPI_ROOT}/etc/rc5.d/S01udhcpd 
-sudo ln -s ../init.d/udhcpd ${RPI_ROOT}/etc/rc6.d/K01udhcpd 
+sudo cp ${tob_repo}/service-scripts/bundle_files/etc/udhcpd.conf ${RPI_ROOT}/etc/
+sudo ln -fs ../init.d/udhcpd ${RPI_ROOT}/etc/rc0.d/K01udhcpd
+sudo ln -fs ../init.d/udhcpd ${RPI_ROOT}/etc/rc1.d/K01udhcpd
+sudo ln -fs ../init.d/udhcpd ${RPI_ROOT}/etc/rc2.d/S01udhcpd
+sudo ln -fs ../init.d/udhcpd ${RPI_ROOT}/etc/rc3.d/S01udhcpd
+sudo ln -fs ../init.d/udhcpd ${RPI_ROOT}/etc/rc4.d/S01udhcpd
+sudo ln -fs ../init.d/udhcpd ${RPI_ROOT}/etc/rc5.d/S01udhcpd
+sudo ln -fs ../init.d/udhcpd ${RPI_ROOT}/etc/rc6.d/K01udhcpd
 
 echo "Setting up static IP address for conference"
-sudo rm ${RPI_ROOT}/etc/systemd/system/dhcpcd.service.d/wait.conf
-sudo cp bundle_files/etc/network/interfaces ${RPI_ROOT}/etc/network/
+sudo rm -f ${RPI_ROOT}/etc/systemd/system/dhcpcd.service.d/wait.conf
+sudo cp ${tob_repo}/service-scripts/bundle_files/etc/network/interfaces ${RPI_ROOT}/etc/network/
 
 echo "Installing initial Breakout_Trust_Onboard_SDK for examples and documentation"
 sudo rm -rf ${RPI_ROOT}/home/pi/Breakout_Trust_Onboard_SDK
-tob_tmp_dir=$(mktemp -d)
-git clone "$TOB_REPO" --branch ${TOB_BRANCH} --recursive ${tob_tmp_dir}
-sudo mv ${tob_tmp_dir} ${RPI_ROOT}/home/pi/Breakout_Trust_Onboard_SDK
+sudo git clone ${tob_repo} --branch ${TOB_BRANCH} ${RPI_ROOT}/home/pi/Breakout_Trust_Onboard_SDK
 pi_uname=$(cat ${RPI_ROOT}/etc/passwd | grep '^pi:' | cut -d ':' -f 3)
 pi_grp=$(cat ${RPI_ROOT}/etc/passwd | grep '^pi:' | cut -d ':' -f 4)
 sudo chown -R ${pi_uname}:${pi_grp} ${RPI_ROOT}/home/pi/Breakout_Trust_Onboard_SDK
 
-sudo umount ${RPI_BOOT}
-sudo umount ${RPI_ROOT}
-sudo losetup -d ${loop_device}
-rmdir ${RPI_BOOT}
-rmdir ${RPI_ROOT}
+echo "Successfully created image at ${raspbian_image}"
