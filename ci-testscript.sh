840ffdcbad520f46502f7962ef4376d46989880e
Anton Gerasimov <agerasimov@twilio.com>
2019-07-08 15:07:16 +0200

    Add test script for CI server to run
    Signed-off-by: Anton Gerasimov <agerasimov@twilio.com>

---
+++ ci-testscript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p cmake
+cd cmake
+cmake -DPCSC_SUPPORT=ON -DSIGNING_SUPPORT=ON -DMBEDTLS_SUPPORT=ON -DBUILD_SHARED=ON -DBUILD_TESTS=ON ..
+make
+
+# Allow overriding modems.csv locally to test this script
+#   modems.csv.local is not used in the CI process
+
+if [ -f ../tests/modems.csv.local ]; then
+	MODEMS_CSV_FILE=../tests/modems.csv.local
+else
+	MODEMS_CSV_FILE=../tests/modems.csv
+fi
+
+../tests/test_runner.py ${MODEMS_CSV_FILE} bin/trust_onboard_sdk_tests bin/trust_onboard_ll_tests
+
+
