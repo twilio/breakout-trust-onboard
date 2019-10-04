9e9f5ad949cc969fd06b1dcd9aa6d8b297285e1a
Randy Beiter <rbeiter@twilio.com>
2019-10-04 14:03:01 +0200

    Add gcov coverage collecting

--- ci-testscript.sh
+++ ci-testscript.sh
@@ -3,7 +3,7 @@
 mkdir -p cmake
 mkdir -p install_prefix
 cd cmake
-cmake -DPCSC_SUPPORT=ON -DOPENSSL_SUPPORT=ON -DMBEDTLS_SUPPORT=ON -DBUILD_SHARED=ON -DBUILD_TESTS=ON -DCMAKE_INSTALL_PREFIX=../install_prefix ..
+cmake -DPCSC_SUPPORT=ON -DOPENSSL_SUPPORT=ON -DMBEDTLS_SUPPORT=ON -DBUILD_SHARED=ON -DBUILD_TESTS=ON -DBUILD_TESTS_COVERAGE=ON -DCMAKE_INSTALL_PREFIX=../install_prefix ..
 make
 make install
 
@@ -18,4 +18,7 @@ fi
 
 ../tests/scripts/test_runner.py ${MODEMS_CSV_FILE} bin/trust_onboard_sdk_tests bin/trust_onboard_ll_tests ../tests/tls_lib_test.sh
 
-
+gcov ./tests/CMakeFiles/trust_onboard_sdk_tests.dir/BreakoutTrustOnboardSDKTests.cpp.gcno ./tests/CMakeFiles/trust_onboard_sdk_tests.dir/BreakoutTrustOnboardLLTests.cpp.gcno ./CMakeFiles/TwilioTrustOnboard.dir/src/BreakoutTrustOnboardSDK.cpp.gcno
+lcov --capture --directory .. --output-file raw_main_coverage.info
+lcov --remove raw_main_coverage.info '*/catch.hpp' '/usr/*' --output-file main_coverage.info
+lcov --list main_coverage.info
