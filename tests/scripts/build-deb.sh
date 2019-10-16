55d5c84269ee3d80d68ff523c3ca9c6c32a6ec97
Anton Gerasimov <agerasimov@twilio.com>
2019-10-16 15:56:28 +0200

    Deploy version tags as debian packages to Bintray
    Signed-off-by: Anton Gerasimov <agerasimov@twilio.com>

---
+++ tests/scripts/build-deb.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -e
+
+if [ $EUID -ne 0 ]; then
+	echo "This tool must be run as root"
+	exit 1
+fi
+
+if [[ ${TRAVIS_TAG} == v* ]]; then
+	VERSION=${TRAVIS_TAG:1}
+else
+	echo "Not in a tag, exit"
+	exit 1
+fi
+
+mkdir -p debs_to_deploy
+git clone https://github.com/oytis/raspbian-deb-builder
+
+pushd ./raspbian-deb-builder
+
+DEBS=$(./cross-build.sh buster breakout-tob-sdk:${TRAVIS_TAG}:${VERSION})
+echo "DEBS IS ${DEBS}"
+
+popd
+
+for d in ${DEBS}; do
+	mv ${d} debs_to_deploy
+done
+
+NUM_DEBS=$(ls -1q debs_to_deploy/* | wc -l)
+
+if [ ${NUM_DEBS} -ne 1 ]; then
+	echo "Exactly one debian file expected"
+	ls -1q debs_to_deploy/*
+	exit 1
+fi
+
+cat >deploy.json <<EOF
+{
+  "package": {
+    "name":"trust-onboard-sdk",
+    "repo":"wireless",
+    "subject":"twilio",
+    "licenses": ["Apache-2.0"],
+    "public_stats": true,
+    "public_download_numbers": false
+  },
+
+  "version": {
+    "name":"${VERSION}",
+    "gpgSign":true
+  },
+
+  "files":
+    [{"includePattern": "debs_to_deploy/(.*\.deb)", "uploadPattern": "pool/main/m/trust-onboard-sdk/\$1",
+      "matrixParams": {
+        "deb_distribution": "buster",
+        "deb_component": "main",
+        "deb_architecture": "armhf"}
+    }],
+  "publish": true
+}
+EOF
